856be1472673a673e8ab201f0869c0b308e918fe
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2025-03-12 05:01:35 +0700

    initialize project with Vite, React, and TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 className="font-bold">Hai GWEH</h1>
+        <p className="text-red-500 font-bold text-3xl">Hello World</p>
+        <Button>Click Me</Button>
+      </div>
+    </>
+  );
+};
+
+export default App;
